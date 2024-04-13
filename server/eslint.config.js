a69fc62662a7e5d82cd9a4f5e89527c83f65c919
Vladyslav Ivashko <vlad.ivashko72@gmail.com>
2024-04-13 17:01:22 +0300

    config eslint/husky

---
+++ server/eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    rules: {
+      "no-var": ["error"],
+      "no-unused-vars": ["error"],
+      "no-use-before-define": ["error"],
+      "no-await-in-loop": ["error"],
+      "no-console": ["warn"],
+    },
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+];
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
+var a = 22;
+console.log(b)
 const app = express();
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
