44344fddbe0c975be2292e2f9bf74226b6939769
vlad-dvlad <vlad.ivashko72@gmail.com>
2024-04-13 23:50:52 +0300

    Dev (#1)
    * add prettier for server
    
    * config eslint/husky
    
    * config
    
    * add first root config
    
    * finish setup husky
    
    * try husky
    
    * refactoring
    
    * try
    
    * try2
    
    * fix issues
    
    * make get req

---
+++ server/eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    rules: {
+      "no-var": ["error"],
+      "no-unused-vars": ["error"],
+      "no-use-before-define": ["error"],
+      "no-await-in-loop": ["warn"],
+      "no-console": ["error"],
+    },
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+];
